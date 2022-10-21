1c71465dfc04d1f04b653d4d9b13cd05160a2969
Pab-83 <gabrielpablo8344@gmail.com>
2022-10-21 15:35:53 -0300

    add brush, fix grid display

--- javascript.js
+++ javascript.js
@@ -1,10 +1,12 @@
 const grid = document.querySelector(".container");
 const resize_grid = document.querySelector("button");
+const input_field = document.querySelector('input');
 
 let gridSize = 16;
 
 function create_grid() {
     let cell_size = grid.clientWidth / gridSize;
+    grid.style = "grid-template-columns: repeat(" + gridSize + "," + cell_size + "px)";
     
     for (let i = 1; i <= gridSize * gridSize; i++) {
         let cell = document.createElement("div");
@@ -13,13 +15,10 @@ function create_grid() {
         cell.style.width = cell_size + "px";
         cell.style.height = cell_size + "px";
     };
-
-    console.log(grid.clientWidth);
 };
 
 function recreate_grid() {
     const cells = document.querySelectorAll('.cell');
-    const input_field = document.querySelector('input');
 
     if (input_field.value <= 0 || input_field.value > 64) {
         window.alert("grid size has to be between 1 and 64!");
@@ -29,9 +28,16 @@ function recreate_grid() {
         gridSize = input_field.value;    
         create_grid()
     };
-}
+};
+
+function brush() {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => cell.addEventListener( 'mouseover', function(e) {
+        e.target.style.backgroundColor = "black";
+    }));
+};
 
 create_grid();
-resize_grid.addEventListener('click', recreate_grid)
-
+resize_grid.addEventListener('click', recreate_grid);
+grid.addEventListener('mouseover', brush);
 
