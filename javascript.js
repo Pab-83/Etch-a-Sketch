f4eda75f0c6b13ba0c1cc56a68b0ce851551058e
Pab-83 <gabrielpablo8344@gmail.com>
2022-10-24 17:57:25 -0300

    add clear button and rainbow mode

--- javascript.js
+++ javascript.js
@@ -1,13 +1,24 @@
 const grid = document.querySelector(".container");
-const resize_grid = document.querySelector("button");
-const input_field = document.querySelector('input');
+
+const resize_grid = document.querySelector("#accept");
+const clear = document.querySelector("#clear");
+
+const input_field = document.querySelector('#size');
+const rainbow_check = document.querySelector('#rainbow');
 
 let gridSize = 16;
 
+
+function random_number_gen(min, max) {
+    return Math.floor(Math.random() * (max - min));
+}
+
 function create_grid() {
     let cell_size = grid.clientWidth / gridSize;
-    grid.style = "grid-template-columns: repeat(" + gridSize + "," + cell_size + "px)";
-    
+    let columns = "grid-template-columns: repeat(" + gridSize + "," + cell_size + "px)";
+    let rows = "grid-template-rows: repeat(" + gridSize + "," + cell_size + "px)";
+    grid.style = columns + ";" + rows;
+
     for (let i = 1; i <= gridSize * gridSize; i++) {
         let cell = document.createElement("div");
         cell.classList.add("cell");
@@ -32,12 +43,29 @@ function recreate_grid() {
 
 function brush() {
     const cells = document.querySelectorAll('.cell');
-    cells.forEach(cell => cell.addEventListener( 'mouseover', function(e) {
-        e.target.style.backgroundColor = "black";
+    let red = random_number_gen(0, 255);
+    let blue = random_number_gen(0, 255);
+    let green = random_number_gen(0, 255);
+    if (!rainbow_check.checked) {
+           cells.forEach(cell => cell.addEventListener( 'mouseover', function(e) {
+            e.target.style.backgroundColor = "black";
+        }));
+    }
+    else {
+        cells.forEach(cell => cell.addEventListener( 'mouseover', function(e) {
+        e.target.style.backgroundColor = "rgb(" + red + "," + blue + "," + green + ")";
     }));
+    };
+};
+
+function clear_grid() {
+    const cells = document.querySelectorAll('.cell');
+
+    cells.forEach(cell => cell.style.backgroundColor = "white");
 };
 
 create_grid();
 resize_grid.addEventListener('click', recreate_grid);
+clear.addEventListener('click', clear_grid);
 grid.addEventListener('mouseover', brush);
 
